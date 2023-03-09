ae0df2788164f36037744c77cad55ecfa907572f
jang-taehyun <71426382+jang-taehyun@users.noreply.github.com>
2023-03-10 00:04:21 +0900

    BFS_4

--- Baekjoon14226.cpp
+++ Baekjoon14226.cpp
@@ -1,81 +1,102 @@
-#include<iostream>
-#include<queue>
- 
-#define endl "\n"
-#define MAX 2000
+/*
+problem : 3가지 방법을 이용해 1부터 end까지 가는 최단 시간을 구해라.
+input : end
+output : 1부터 end까지 가는 최단 시간
+
+idea
+- 1부터 시작하여 level이 시간을 나타내는 tree를 만든다.
+- 각 경우에 대한 조건은 다음과 같다.
+  1) 클립보드에 저장 : 화면에 이모티콘이 1개 이상 존재
+  2) 화면에 붙여넣기 : 클립보드에 이모티콘이 1개 이상 존재
+  3) 화면에 이모티콘 1개 감소 : 화면에 이모티콘이 1개 이상 존재
+*/
+#include <iostream>
+#include <queue>
 using namespace std;
- 
-int S;
-bool Visit[MAX][MAX];
- 
-void Input()
+
+int target;                        // 도착 지점 target
+bool isCheckNode[2001][2001];      // 화면에 first개가 있고 클립보드에 second개가 있는 node 방문 여부
+
+// 범위 계산
+bool isCheckRange(int n1, int n2)
 {
-    cin >> S;
+    if(n1 < 1 || n2 < 1 || n1 > 2000 || n2 > 2000)
+        return false;
+    return true;
 }
- 
+
 int BFS()
 {
-    queue<pair<pair<int, int>, int> > Q;
-    Q.push(make_pair(make_pair(1, 0), 0));
-    Visit[1][0] = true;    // 화면, 클립보드
- 
-    while (Q.empty() == 0)
+    queue<pair<int, int>> nextSearch;        // 다음에 탐색할 node들 nextSearch
+    int minimumTime=0;                       // end에 도착할 때 까지 걸린 시간 minimumTime
+    
+    // 1을 queue에 push 및 방문 처리
+    nextSearch.push(make_pair(1, 0));
+    isCheckNode[1][0] = true;
+    
+    // queue가 빌 때까지 반복
+    while(!nextSearch.empty())
     {
-        int Dis = Q.front().first.first;
-        int Clip = Q.front().first.second;
-        int Time = Q.front().second;
-        Q.pop();
- 
-        if (Dis == S) return Time;
+        // queue의 사이즈만큼 반복
+        int searchCount = nextSearch.size();
         
-        if (Dis > 0 && Dis < MAX)
+        for(int i=0; i<searchCount; i++)
         {
-            //1번 & 3번 조건
-            if (Visit[Dis][Dis] == false)
+            // queue에서 좌표 하나 pop
+            int currentNode = nextSearch.front().first;
+            int currentClipboard = nextSearch.front().second;
+            nextSearch.pop();
+            
+            // end에 도착하면 최소 시간 return
+            if(currentNode == target)
+                return minimumTime;
+            
+            int nextNode, nextClipboard;
+            
+            // 3가지 방법의 결과를 queue에 모두 push
+            if(currentNode >= 1)
             {
-                Visit[Dis][Dis] = true;
-                Q.push(make_pair(make_pair(Dis, Dis), Time + 1));
+                // 클립보드에 복사 연산
+                nextNode = currentNode;
+                nextClipboard = currentNode;
+                if(isCheckRange(nextNode, nextClipboard) && !isCheckNode[nextNode][nextClipboard])
+                {
+                    nextSearch.push(make_pair(nextNode, nextClipboard));
+                    isCheckNode[nextNode][nextClipboard] = true;
+                }
+                
+                // 화면에 이모티콘 1개 삭제 연산
+                nextNode = currentNode-1;
+                nextClipboard = currentClipboard;
+                if(isCheckRange(nextNode, nextClipboard) && !isCheckNode[nextNode][nextClipboard])
+                {
+                    nextSearch.push(make_pair(nextNode, nextClipboard));
+                    isCheckNode[nextNode][nextClipboard] = true;
+                }
             }
- 
-            if (Visit[Dis - 1][Clip] == false)
+
+            // 화면에 붙여넣기 연산
+            nextNode = currentNode+currentClipboard;
+            nextClipboard = currentClipboard;
+            if(currentClipboard >= 1 && isCheckRange(nextNode, nextClipboard) && !isCheckNode[nextNode][nextClipboard])
             {
-                Visit[Dis - 1][Clip] = true;
-                Q.push(make_pair(make_pair(Dis - 1, Clip), Time + 1));
-            }
+                nextSearch.push(make_pair(nextNode, nextClipboard));
+                isCheckNode[nextNode][nextClipboard] = true;
+            }   
         }
- 
-        if (Clip > 0 && Dis+Clip < MAX)
-        {
-            if (Visit[Dis + Clip][Clip] == false)
-            {
-                Visit[Dis + Clip][Clip] = true;
-                Q.push(make_pair(make_pair(Dis + Clip, Clip), Time + 1));
-            }
-        }
+        
+        // 시간 증가
+        minimumTime++;
     }
 }
- 
- 
-void Solution()
+
+int main()
 {
-    int R = BFS();
-    cout << R << endl;
-}
- 
-void Solve()
-{
-    Input();
-    Solution();
-}
- 
-int main(void)
-{
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
- 
-    //freopen("Input.txt", "r", stdin);
-    Solve();
- 
+    // input 입력
+    cin >> target;
+    
+    // 결과 출력
+    cout << BFS();
+    
     return 0;
 }
