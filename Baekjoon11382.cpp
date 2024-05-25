0efe674780b5e8331f6619ac289fc0957c62bc43
jang-taehyun <jth0202@naver.com>
2024-05-25 17:19:03 +0900

    구현

---
+++ Baekjoon11382.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+typedef unsigned long long ULL;
+
+int main()
+{
+    ULL A, B, C;
+    cin >> A >> B >> C;
+
+    cout << A + B + C << '\n';
+
+    return 0;
+}
