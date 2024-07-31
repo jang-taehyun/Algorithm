a06f4f59608e37b72741f7dea47eb4268d65357a
jang-taehyun <71426382+jang-taehyun@users.noreply.github.com>
2024-07-31 09:21:07 +0900

    구현

---
+++ Baekjoon2145.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string input;
+    int tmp=0;
+    
+    while(1)
+    {
+        cin >> input;
+        if("0" == input)
+            break;
+        
+        while(input.length() > 1)
+        {
+            tmp=0;
+            for(int i=0; i<input.length(); i++)
+                tmp += (input[i] - '0');
+            input = to_string(tmp);
+        }
+        
+        cout << input << '\n';
+    }
+    
+    return 0;
+}
