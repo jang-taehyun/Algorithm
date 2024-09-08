5060726aaf0a78262497b6ada353d581130df56d
jang-taehyun <71426382+jang-taehyun@users.noreply.github.com>
2024-09-08 12:00:14 +0900

    구현

---
+++ Baekjoon2845.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int l, p;
+    int arr[5];
+    
+    cin >> l >> p;
+    for(int i=0; i<5; i++)
+        cin >> arr[i];
+    
+    for(int i=0; i<5; i++)
+        cout << (arr[i] - (l * p)) << ' ';
+    
+    return 0;
+}
