b4602a1f6c629f4e24c818202855b8be142fdc59
jang-taehyun <71426382+jang-taehyun@users.noreply.github.com>
2025-06-27 04:17:32 +0900

    구현

---
+++ Baekjoon16394.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n;
+    cin >> n;
+    
+    cout << (n - 1946);
+    
+    return 0;
+}
