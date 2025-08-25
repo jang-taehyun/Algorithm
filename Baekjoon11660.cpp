39a4fe89b3b735b7c4d06aba1e18b2b424658ecd
jang-taehyun <71426382+jang-taehyun@users.noreply.github.com>
2025-08-25 17:35:30 +0900

    prefix sum

--- Baekjoon11660.cpp
+++ Baekjoon11660.cpp
@@ -1,56 +1,31 @@
 #include <iostream>
 using namespace std;
 
-int board[1024][1024] = {0, };
-int arr[1024][1024] = { 0, };
+int arr[1025][1025] = { 0, };
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     
-    int n, cnt, tmp;
+    int n, cnt;
     int x1, y1, x2, y2;
     int res;
     
     cin >> n >> cnt;
     
-    for(int i=0; i<n; ++i)
-        for(int j=0; j<n; ++j)
-            cin >> board[i][j];
-    
-    for(int i=0; i<n; ++i)
-    {
-        tmp = 0;
-        for(int j=0; j<n; ++j)
+    for(int i=1; i<=n; ++i)
+        for(int j=1; j<=n; ++j)
         {
-            tmp += board[i][j];
-            
-            if(!i)
-                arr[i][j] = tmp;
-            else
-                arr[i][j] = arr[i-1][j] + tmp;
+            cin >> arr[i][j];
+            arr[i][j] += arr[i-1][j] + arr[i][j-1] - arr[i-1][j-1];
         }
-    }
+            
     
     while(cnt--)
     {
         cin >> x1 >> y1 >> x2 >> y2;
-        
-        --x1;
-        --y1;
-        --x2;
-        --y2;
-        
-        res = arr[x2][y2];
-        
-        if(x1-1 >= 0)
-            res -= arr[x1-1][y2];
-        if(y1-1 >= 0)
-            res -= arr[x2][y1-1];
-        if(x1-1 >= 0 && y1-1 >= 0)
-            res += arr[x1-1][y1-1];
-        
+        res = arr[x2][y2] - arr[x1-1][y2] - arr[x2][y1-1] + arr[x1-1][y1-1];
         cout << res << '\n';
     }
     
