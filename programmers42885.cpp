c6d7e1025e0197babab24e32e0f8ac71e3313b16
jang-taehyun <71426382+jang-taehyun@users.noreply.github.com>
2025-09-26 15:21:38 +0900

    two pointer

---
+++ programmers42885.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> people, int limit) {
+    int answer = 0;
+    int s=0, e=people.size()-1;
+    
+    sort(people.begin(), people.end());
+    
+    while(s < e)
+    {
+        if(people[s] + people[e] <= limit)
+            s++;
+        
+        ++answer;
+        --e;
+    }
+    
+    if(s == e)
+        ++answer;
+    
+    return answer;
+}
