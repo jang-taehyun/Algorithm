3d310643876877634a00c417d15a1aefc8e68f22
jang-taehyun <71426382+jang-taehyun@users.noreply.github.com>
2025-10-06 23:51:12 +0900

    sql 연습

---
+++ solution131535.sql
@@ -0,0 +1,5 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/131535
+-- 코드를 입력하세요
+SELECT COUNT(*) AS USERS
+FROM USER_INFO 
+WHERE JOINED BETWEEN '2021-01-01' AND '2021-12-31' AND AGE BETWEEN 20 AND 29
