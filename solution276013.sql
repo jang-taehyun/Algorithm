6166ed641d362bfcf3aa68c7bd7f91995ee21db0
jang-taehyun <71426382+jang-taehyun@users.noreply.github.com>
2025-10-07 22:09:52 +0900

    sql 연습

---
+++ solution276013.sql
@@ -0,0 +1,6 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/276013
+-- 코드를 작성해주세요
+SELECT ID, EMAIL, FIRST_NAME, LAST_NAME
+FROM DEVELOPER_INFOS
+WHERE SKILL_1 LIKE 'Python' OR SKILL_2 LIKE 'Python' OR SKILL_3 LIKE 'Python'
+ORDER BY ID
