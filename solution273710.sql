960156d2d98c9f0a910ac6fc3cf1c07cee33e930
jang-taehyun <71426382+jang-taehyun@users.noreply.github.com>
2025-10-15 14:46:17 +0900

    sql 연습

---
+++ solution273710.sql
@@ -0,0 +1,10 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/273710
+-- 코드를 작성해주세요
+SELECT ITEM_ID, ITEM_NAME
+FROM ITEM_INFO 
+WHERE ITEM_ID IN (
+    SELECT ITEM_ID
+    FROM ITEM_TREE 
+    WHERE PARENT_ITEM_ID IS NULL
+)
+ORDER BY ITEM_ID ASC
