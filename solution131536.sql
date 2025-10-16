4844707cc162043e6797c56e12aa10fd53707f17
jang-taehyun <71426382+jang-taehyun@users.noreply.github.com>
2025-10-16 15:46:46 +0900

    sql 연습

---
+++ solution131536.sql
@@ -0,0 +1,7 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/131536
+-- 코드를 입력하세요
+SELECT USER_ID, PRODUCT_ID
+FROM ONLINE_SALE
+GROUP BY USER_ID, PRODUCT_ID
+HAVING COUNT(*) >= 2
+ORDER BY USER_ID ASC, PRODUCT_ID DESC
