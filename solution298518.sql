c9d7b9aa519e4c10f94b03b1830576824422788a
jang-taehyun <71426382+jang-taehyun@users.noreply.github.com>
2025-10-17 23:34:53 +0900

    sql 연습

---
+++ solution298518.sql
@@ -0,0 +1,10 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/298518
+-- 코드를 작성해주세요
+SELECT COUNT(ID) AS 'FISH_COUNT'
+FROM FISH_INFO 
+WHERE FISH_TYPE IN
+(
+    SELECT FISH_TYPE
+    FROM FISH_NAME_INFO
+    WHERE FISH_NAME LIKE 'BASS' OR FISH_NAME LIKE 'SNAPPER'
+)
